92e93702cba0407ef918f796a2125c0df64ef0d7
DrashtiBorad <djborad0890@gmail.com>
2025-02-02 06:44:37 +0530

    add postgres database configuration.

--- index.ts
+++ index.ts
@@ -1,12 +1,23 @@
 import "reflect-metadata";
 import express from "express";
+import cors from "cors";
+import env from "dotenv";
+import { appDataSource } from "./src/config/database";
 const app = express();
 const port = 3003;
+app.use(cors());
+env.config();
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Server started Successfully." });
 });
 
-app.listen(port, () => {
-  console.log(`server started at ${port}`);
-});
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log("connected");
+    app.listen(port, () => {
+      console.log(`server starting at port ${port}`);
+    });
+  })
+  .catch((err) => console.log("err", err));
---
+++ src/config/database.ts
@@ -0,0 +1,16 @@
+import { DataSource } from "typeorm";
+import env from "dotenv";
+
+env.config();
+
+export const appDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.PG_HOST,
+  port: 5432,
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PASSWORD,
+  database: process.env.DATABASE,
+  entities: [],
+  synchronize: true,
+  logging: true,
+});
