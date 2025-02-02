fd543b9fb122ff80817352664733cf792c14eeb3
DrashtiBorad <djborad0890@gmail.com>
2025-02-02 07:48:49 +0530

    add router configuration

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@ import "reflect-metadata";
 import express from "express";
 import cors from "cors";
 import env from "dotenv";
-import { DataSource } from "typeorm";
+import { appDataSource } from "./src/config/database";
+import { router } from "./src/api/route";
 const app = express();
 const port = 3003;
 app.use(cors());
@@ -13,23 +14,8 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+app.use(router);
 env.config();
-
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Server started Successfully." });
-});
-
-export const appDataSource = new DataSource({
-  type: "postgres",
-  host: process.env.PG_HOST,
-  port: 5432,
-  username: process.env.PG_USERNAME,
-  password: process.env.PG_PASSWORD,
-  database: process.env.DATABASE,
-  entities: [],
-  synchronize: true,
-  logging: true,
-});
 
 appDataSource
   .initialize()
