025434b6ae40dc913020c9357d30d30f9e1f87bb
DrashtiBorad <djborad0890@gmail.com>
2025-02-09 21:00:59 +0530

    add consoles.

--- index.ts
+++ index.ts
@@ -16,7 +16,18 @@ app.use(
 );
 app.use(router);
 env.config();
-
+console.log({
+  pg_Host: process.env.PG_HOST,
+  username: process.env.PG_USERNAME,
+  password: process.env.PG_PASSWORD,
+  database: process.env.DATABASE,
+  jsonKey: process.env.JSON_KEY,
+  email: process.env.AUTH_USER_EMAIL,
+  json_password: process.env.AUTH_USER_PASSWORD,
+  bucketName: process.env.BUCKET_NAME,
+  awsKey: process.env.AWS_ACCESSKEY_ID,
+  seceretKey: process.env.AWS_SECRET_ACCESS_KEY,
+});
 appDataSource
   .initialize()
   .then(() => {
