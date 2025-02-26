6a66c057eaf5949525195c14ee5722bb332ce3a7
DrashtiBorad <djborad0890@gmail.com>
2025-02-26 15:05:28 +0530

    add db connection string

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,7 +5,7 @@ env.config();
 
 export const appDataSource = new DataSource({
   type: "postgres",
-  // host: process.env.PG_HOST,
+  url: process.env.DATABASE_URL,
   port: 5432,
   username: process.env.PG_USERNAME,
   password: process.env.PG_PASSWORD,
