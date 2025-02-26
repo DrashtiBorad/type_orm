4f51ff7c50e346df685056cdf5a5a0b4701def20
DrashtiBorad <djborad0890@gmail.com>
2025-02-26 15:46:13 +0530

    add testing route

--- src/api/route.ts
+++ src/api/route.ts
@@ -31,6 +31,9 @@ import {
 
 export const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send({ message: "Server started successfully." });
+});
 // User Router
 
 router.post("/register", registration);
