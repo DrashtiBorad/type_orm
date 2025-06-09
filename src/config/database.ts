e86262e3dba54be10d9e191ec13fe0d99e039447
drashtiBorad <djborad0890@gmail.com>
2025-06-09 07:35:07 +0530

    fix: remove hardcoded host from database configuration

--- src/config/database.ts
+++ src/config/database.ts
@@ -8,7 +8,6 @@ export const appDataSource = new DataSource({
   type: "postgres",
   url: process.env.DATABASE_URL,
   port: 5432,
-  host: "localhost",
   username: process.env.PG_USERNAME,
   password: process.env.PG_PASSWORD,
   database: process.env.DATABASE,
